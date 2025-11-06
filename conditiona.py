2a33df57bab7da4283e113e89b05a3775c7b5ec6
unknown <akhilpandey1090@gmail.com>
2025-11-06 23:49:01 +0530

    Python code

---
+++ conditiona.py
@@ -0,0 +1,28 @@
+"""age=int(input("enter the age :"))
+if(age>=18):
+    print(" age is the above of life:")
+    print("this is the best age of representing to good experience:")
+elif(age<=0):
+    print("the invalid age :")
+else:
+    print("age is the below:")
+print("ending from programm:")
+a=int(input("enter the age:"))
+if(a>=18):
+    print("the age is valid :")
+    print("yas:")
+else:
+    print("number is not valid for the age :")
+    print("invalid age :")
+print("ending the programm here :")"""
+a=int(input("enter the age:"))
+if(a%2==0):
+    print("even")
+else:
+    print("odd:")
+if(a>=18):
+    print("age is meture:")
+else:
+    print("not valid age for mature:")
+print("exiting cod")
+print("this code made by layak pandey:")
